c68eaa0dcfdc63b852d8f8f54ac98ecdca993f38
Deifante Walters <deifante@gmail.com>
2012-04-10 11:27:05 -0600

    Now we can handle rows with no village. The sample csv file has been updated to reflect this.

--- water_scraper.py
+++ water_scraper.py
@@ -2,8 +2,10 @@
 # coding: utf-8
 
 """
-water_scraper is a Python library for reading a specified csv file, then creating directories
-for the related images.
+water_scraper is a Python library for reading a specified csv file.
+The csv file contains a Village, filter and image url. This class
+will retrieve the image name it after it's filter #  and put it in
+a directory for it's village.
 """
 import os
 import csv
@@ -13,28 +15,34 @@ __author__ = "Deifante Jay Walters"
 __version__ = "0.0.1"
 
 class WaterScraper(object):
-    """Will read the scv, create directories and place the appropriate images in them."""
+    """Will read the csv, create directories and place the appropriate images in them."""
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, dest_folder = 'dumps'):
         self.__csv_file_name = file_name
-        self.__destination_folder = 'dest'
+        self.__destination_folder = dest_folder
 
     def scrape(self):
         water_reader = csv.reader(open(self.__csv_file_name, 'rb'))
         column_names = water_reader.next()
-        dump_dir = os.path.join(os.getcwd(), 'dumps')
-        
+        dump_dir = os.path.join(os.getcwd(), self.__destination_folder)
+
         created_directories = {}
         for row in water_reader:
             if row[0] not in created_directories:
-                new_directory = os.path.join(dump_dir, row[0])
+                if len(row[0]) == 0:
+                    new_directory = os.path.join(dump_dir, 'Unknown Village')
+                else:
+                    new_directory = os.path.join(dump_dir, row[0])
                 created_directories[row[0]] = new_directory
-                os.makedirs(new_directory)
-        
+                if not os.path.exists(new_directory):
+                    os.makedirs(new_directory)
+
+            # Because the file extensions end in /.jpg it takes a little more
+            # work than usual to get the file extension.
             extension = row[2][-3:]
             if extension != 'jpg' and len(os.path.splitext(row[2])):
                 extension = os.path.splitext(row[2])
-                
+
             new_filename = "{0}.{1}".format(row[1], extension)
             urllib.urlretrieve(row[2], os.path.join(created_directories[row[0]],new_filename))
 

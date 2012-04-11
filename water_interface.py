101491ada2bd9f58f8075b6e6606f3d08bb6ecb6
Deifante Walters <deifante@gmail.com>
2012-04-11 15:47:08 -0600

    close to done. now to make a windows exe...

--- water_interface.py
+++ water_interface.py
@@ -15,27 +15,40 @@ class WaterInterface(object):
 
     def __init__(self, master):
         frame = Frame(master)
-        frame.pack()
+        self.__master = master
+        master.title("Steve's Water App")
+        frame.grid(column=0, row=0, sticky=(N, W, E, S))
+        frame.columnconfigure(0, weight=1)
+        frame.rowconfigure(0, weight=1)
+        master.bind('<Return>', self.go)
 
-        Label(frame, text='Source (.csv)').pack(side=LEFT)
-        self.__source = Text(frame, height=1)
-        self.__source.pack(side=LEFT)
+        Label(frame, text='Source (.csv)').grid(column=1, row=1, sticky=(W, E))
+        self.__source = Entry(frame)
+        self.__source.grid(column=2, row=1, sticky=(W, E))
 
-        Label(frame, text='Destination (folder)').pack(side=LEFT)
-        self.__dest = Text(frame, height=1)
-        self.__dest.pack(side=LEFT)
+        Label(frame, text='Destination (folder)').grid(column=1, row=2, sticky=(W,E))
+        self.__dest = Entry(frame)
+        self.__dest.grid(column=2, row=2, sticky=(W,E))
 
-        self.button = Button(frame, text="Go", command=self.go)
-        self.button.pack(side=RIGHT)
+        self.button = Button(frame, text="Go", command=self.go).grid(column=2, row=3, sticky=(W,E))
+
+        for child in frame.winfo_children():
+            child.grid_configure()
 
     def go(self):
-        source = self.__source.get(1.0, END).strip()
-        dest = self.__dest.get(1.0, END).strip()
+        source = self.__source.get().strip()
+        dest = self.__dest.get().strip()
 
         if len(source) > 0 and len(dest) > 0:
             scraper = WaterScraper(source, dest)
+            self.update_title('Working...')
             scraper.scrape()
+            self.update_title('Done!')
 
+    def update_title(self, new_title):
+        if not new_title.startswith("Steve's Water App"):
+            new_title = "Steve's Water App - {0}".format(new_title)
+        self.__master.title(new_title)
 
 if __name__ == "__main__":
     root = Tk()

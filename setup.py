abf33e52a6160c9b932b14ad049b7cfa48cc103a
Deifante Walters <deifante@gmail.com>
2012-04-12 15:56:39 -0600

    Slight mods to setup.py to see if I can get this thing working.

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
-import py2exe
+#import py2exe
 
-setup(console=['water_interface.py', 'water_scraper.py'])
+setup(name         = 'Water Filter Pictures',
+      author       = 'Deifante Jay Walters',
+      author_email = 'deifante@gmail.com',
+      description  = 'Uses csv input to organise and download a set of jpgs.',
+      py_modules   = ['water_interface', 'water_scraper'],
+      requires     = ['Tkinter', 'csv', 'urllib', 'os'],
+      version      = '0.1',
+      provides     = ['water_scraper', 'water_interface'],
+      url          = 'http://www.deifante.com/'
+      )
